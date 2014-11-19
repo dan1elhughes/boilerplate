8a1de86bb8234b14cdb0f723199fe922fa8f6c28
Dan Hughes <danhughes.dh+github@gmail.com>
2014-11-19 21:39:39 +0000

    Remove tiny fonts, enlarge checkmark

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,6 +3,7 @@
 	<div class="wrap align-center">
 		<h1>Success!</h1>
 		<p>Boilerplate cloned successfully.</p>
+		<span class="fontsize-4"><i class="fa fa-check"></i></span>
 	</div>
 </div>
 <?php include "./includes/footer.php"; ?>
