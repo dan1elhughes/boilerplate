de2a9a484fc606bc9cfc519a6533f4d2e2d5f79d
Dan Hughes <danhughes.dh+github@gmail.com>
2014-12-28 20:28:44 +0000

    Enlarge default text

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,8 +1,8 @@
 <?php include "./includes/header.php"; ?>
 <div class="block">
 	<div class="wrap align-center">
-		<h1>Success!</h1>
-		<p>XES Boilerplate installed successfully.</p>
+		<h1 class="fontsize-huge">Success!</h1>
+		<p class="fontsize-normal">XES Boilerplate installed successfully.</p>
 		<p><a class="fontsize-xhuge faint" href="https://github.com/dan1elhughes/boilerplate.git"><i class="fa fa-github"></i></a></p>
 	</div>
 </div>
