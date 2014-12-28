1cbfea37c47d38409cf7301385deaa6b2236eeb6
Dan Hughes <danhughes.dh+github@gmail.com>
2014-12-28 21:43:46 +0000

    Add front page to responsive grid

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,9 +1,15 @@
 <?php include "./includes/header.php"; ?>
 <div class="block">
-	<div class="wrap align-center">
-		<h1 class="fontsize-huge">Success!</h1>
-		<p class="fontsize-large">XES Boilerplate installed successfully.</p>
-		<p><a class="fontsize-xhuge faint" href="https://github.com/dan1elhughes/boilerplate.git"><i class="fa fa-github"></i></a></p>
-	</div>
+	<div class="container align-center">
+		<div class="row">
+			<h1 class="fontsize-huge twelve columns">Success!</h1>
+		</div>	
+		<div class="row">
+			<p class="fontsize-large twelve columns">XES Boilerplate installed successfully.</p>
+		</div>	
+		<div class="row">
+			<p><a class="fontsize-huge faint twelve columns" href="https://github.com/dan1elhughes/boilerplate.git"><i class="fa fa-github"></i></a></p>
+		</div>	
+	</div>	
 </div>
 <?php include "./includes/footer.php"; ?>
