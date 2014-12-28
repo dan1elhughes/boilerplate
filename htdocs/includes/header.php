1b5f73e9fd4d472d2cdc93380c31563a1248cca0
Dan Hughes <danhughes.dh+github@gmail.com>
2014-12-28 18:46:16 +0000

    Wrap site in containers

--- htdocs/includes/header.php
+++ htdocs/includes/header.php
@@ -91,9 +91,10 @@ function thumb($src, $width) {
 		<title><!--TITLE--></title>
 	</head>
 	<body>
-		<!--[if IE]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
-		<header>
-			<div class='wrap'>
-			</div>
-		</header>
-	<div id="content"> <!-- Begin page content -->
+		<div class="float-container site-container">
+			<!--[if IE]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
+			<header>
+				<div class='wrap'>
+				</div>
+			</header>
+			<div id="content"> <!-- Begin page content -->
